437dae9e0b2873af92f53af8fee67b2028ee23dd
Cédric Skwar <cdrc@skwar.me>
2023-05-02 08:44:41 +0200

    Removed the TransmitDataText func. started my connect function.

--- app.go
+++ app.go
@@ -50,41 +50,6 @@ func (a *App) CreatePeerConnection() *webrtc.PeerConnection {
 	return peerConnection
 }
 
-func (a *AllConfig) TransmitDataText(peerConnection *webrtc.PeerConnection, data string) {
-	a.Config.verbose = true // Atleast for now for testing and developing the things, maybe later we can make it a flag, when we release it.
-	m := &utils.MessengerUtils{
-		Verbose: a.Config.verbose,
-	}
-	m.PrintInfo("Started the connection process")
-
-	protcol := "tcp"
-	order := true
-	var maxReTransmission uint16 = 5000
-
-	dataChannelInit := webrtc.DataChannelInit{
-		Ordered:        &order,
-		MaxRetransmits: &maxReTransmission,
-		Protocol:       &protcol,
-	}
-	// Create a datachannel with label 'data'
-	dataChannel, err := peerConnection.CreateDataChannel("text", &dataChannelInit)
-	if err != nil {
-		utils.PrintError("During the creation of the data channel an error ocured", err)
-		panic(err)
-	}
-
-	// Register channel opening handling
-	dataChannel.OnOpen(func() {
-		// Send the data
-		dataChannel.SendText(data)
-	})
-
-	// Register text message handling
-	dataChannel.OnMessage(func(msg webrtc.DataChannelMessage) {
-		println("Received message: " + string(msg.Data))
-	})
-}
-
 func (a *App) Disconnect() {
 	// Close the connection
 	a.ctx.Done()
@@ -94,4 +59,18 @@ func (w *WebRTCConfig) Constructor() {
 	w.connected = new(bool)
 	w.localMessage = new(string)
 	w.remoteMessage = new(string)
+}
+
+func (a *AllConfig) Connect() {
+	a.Config.verbose = true
+	m := &utils.MessengerUtils{
+		Verbose: a.Config.verbose,
+	}
+
+	m.PrintInfo("Connecting...")
+	dataChannelParameters := webrtc.DataChannelParameters{
+		Label:    "data",
+		Ordered:  true,
+		Protocol: "tcp",
+	}
 }
