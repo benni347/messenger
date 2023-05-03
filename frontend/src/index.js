70fe65ea10e11cbc68019588cb95d4f8f0857fd6
Cédric Skwar <cdrc@skwar.me>
2023-05-03 10:05:14 +0200

    Class
    Crated the Messaging class for a custom element.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,11 @@
 "use strict";
 
 import { Connect, CreatePeerConnection Dissconnect, ReceiveLocalMessage, ReceiveRemoteMessage, Send } from './wailsjs/go/main/App.js';
+
+class Messaging {
+  constructor() {
+    super();
+    this.localMessage = "";
+    this.remoteMessage = "";
+  }
+}
