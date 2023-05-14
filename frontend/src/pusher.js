f3485172f5a22442233d55245a4ea80893900aa4
Cédric Skwar <cdrc@skwar.me>
2023-05-14 22:13:31 +0200

    Updated the pusher.js
    Added a pusher client function to retrive messages, but currently it is not able to push msgs.

--- frontend/src/pusher.js
+++ frontend/src/pusher.js
@@ -6,6 +6,8 @@ let appId;
 let appKey;
 let clusterId;
 let appSecret;
+let channelName = "my-channel";
+const messageLog = document.getElementById("message-log");
 
 RetrieveEnvValues().then((env) => {
   console.log(`Env: ${env}`);
@@ -16,6 +18,21 @@ RetrieveEnvValues().then((env) => {
   console.info(`App Id: ${appId}`);
   console.info(`App Key: ${appKey}`);
   console.info(`App Cluster: ${clusterId}`);
+  PusherClient();
 });
 
-console.info(`App Cluster: ${clusterId}`);
+// Enable pusher logging - don't include this in production
+Pusher.logToConsole = true;
+
+function PusherClient() {
+  const pusher = new Pusher(appKey, {
+    cluster: clusterId,
+  });
+
+  const channel = pusher.subscribe(channelName);
+  channel.bind("my-event", function(data) {
+    console.info(`Pusher data: ${JSON.stringify(data)}`);
+    messageLog.append(`${JSON.stringify(data)}\n`);
+    alert(JSON.stringify(data));
+  });
+}
