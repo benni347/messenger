35fd0d0723f10edd5961aa58feba20d26ccf9255
Cédric Skwar <cdrc@skwar.me>
2023-05-15 22:15:49 +0200

    Updated the retrive env value.

--- frontend/src/supabase.js
+++ frontend/src/supabase.js
@@ -2,6 +2,10 @@
 
 import { RetrieveEnvValues } from "../wailsjs/go/main/App.js";
 
+// FIXME: Uncaught SyntaxError: ambiguous indirect export: createClient
+// When using "import createClient from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'" it kind of works.
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2";
+
 const options = {
   db: {
     schema: "public",
@@ -15,18 +19,29 @@ const options = {
     headers: { "x-my-custom-header": "my-app-name" },
   },
 };
-const supabaseUrl = RetrieveEnvValues().then((env) => {
-  return env.supaBaseUrl;
-});
-const supabaseKey = RetrieveEnvValues().then((env) => {
-  return env.supaBaseApiKey;
-});
 
-// FIXME: Uncaught ReferenceError: createClient is not defined
+let supabaseKey;
+let supabaseUrl;
+let supabase;
+// Solution for the error:
+// https://stackoverflow.com/a/74261887
+// -FIX-ME-: Uncaught ReferenceError: createClient is not defined
 // Tried fixes:
 //  - importing the createClient function from the supabase-js module directly via the cdn in this file
 //  - importing it via "import { createClient } from "@supabase/supabase-js";"
-const supabase = createClient(supabaseUrl, supabaseKey, options);
+
+RetrieveEnvValues().then((env) => {
+  console.log(`Env: ${env}`);
+  supabaseKey = env.supaBaseApiKey;
+  supabaseUrl = env.supaBaseUrl;
+  console.info(`Supabase Key: ${supabaseKey}`);
+  console.info(`Supabase URL: ${supabaseUrl}`);
+  console.info(`Supabase options: ${JSON.stringify(options)}`);
+  supabase = createClient(supabaseUrl, supabaseKey, options);
+});
+
+console.info(`Supabase URL: ${supabaseUrl}`);
+console.info(`Supabase Key: ${supabaseKey}`);
 
 async function signUp(mail, password) {
   const { data, error } = await supabase.auth.signUp({
