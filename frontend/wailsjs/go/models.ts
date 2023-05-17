01e49ff540f802718eab0cc277fa94dd076cdd75
Cedric Skwar <cdrc@skwar.me>
2023-05-17 10:23:57 +0200

    Removed the db functions

--- frontend/wailsjs/go/models.ts
+++ frontend/wailsjs/go/models.ts
@@ -1,121 +1,27 @@
 export namespace main {
-  export class Message {
-    msg: string;
-    // Go type: time
-    date: any;
-    sender: string;
+	
+	export class Config {
+	    appId: string;
+	    appSecret: string;
+	    appKey: string;
+	    clusterId: string;
+	    supaBaseApiKey: string;
+	    supaBaseUrl: string;
+	
+	    static createFrom(source: any = {}) {
+	        return new Config(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.appId = source["appId"];
+	        this.appSecret = source["appSecret"];
+	        this.appKey = source["appKey"];
+	        this.clusterId = source["clusterId"];
+	        this.supaBaseApiKey = source["supaBaseApiKey"];
+	        this.supaBaseUrl = source["supaBaseUrl"];
+	    }
+	}
 
-    static createFrom(source: any = {}) {
-      return new Message(source);
-    }
-
-    constructor(source: any = {}) {
-      if ("string" === typeof source) source = JSON.parse(source);
-      this.msg = source["msg"];
-      this.date = this.convertValues(source["date"], null);
-      this.sender = source["sender"];
-    }
-
-    convertValues(a: any, classs: any, asMap: boolean = false): any {
-      if (!a) {
-        return a;
-      }
-      if (a.slice) {
-        return (a as any[]).map((elem) => this.convertValues(elem, classs));
-      } else if ("object" === typeof a) {
-        if (asMap) {
-          for (const key of Object.keys(a)) {
-            a[key] = new classs(a[key]);
-          }
-          return a;
-        }
-        return new classs(a);
-      }
-      return a;
-    }
-  }
-  export class Chat {
-    chatid: number;
-    msgs: Message[];
-
-    static createFrom(source: any = {}) {
-      return new Chat(source);
-    }
-
-    constructor(source: any = {}) {
-      if ("string" === typeof source) source = JSON.parse(source);
-      this.chatid = source["chatid"];
-      this.msgs = this.convertValues(source["msgs"], Message);
-    }
-
-    convertValues(a: any, classs: any, asMap: boolean = false): any {
-      if (!a) {
-        return a;
-      }
-      if (a.slice) {
-        return (a as any[]).map((elem) => this.convertValues(elem, classs));
-      } else if ("object" === typeof a) {
-        if (asMap) {
-          for (const key of Object.keys(a)) {
-            a[key] = new classs(a[key]);
-          }
-          return a;
-        }
-        return new classs(a);
-      }
-      return a;
-    }
-  }
-  export class Chats {
-    chats: Chat[];
-
-    static createFrom(source: any = {}) {
-      return new Chats(source);
-    }
-
-    constructor(source: any = {}) {
-      if ("string" === typeof source) source = JSON.parse(source);
-      this.chats = this.convertValues(source["chats"], Chat);
-    }
-
-    convertValues(a: any, classs: any, asMap: boolean = false): any {
-      if (!a) {
-        return a;
-      }
-      if (a.slice) {
-        return (a as any[]).map((elem) => this.convertValues(elem, classs));
-      } else if ("object" === typeof a) {
-        if (asMap) {
-          for (const key of Object.keys(a)) {
-            a[key] = new classs(a[key]);
-          }
-          return a;
-        }
-        return new classs(a);
-      }
-      return a;
-    }
-  }
-  export class Config {
-    appId: string;
-    appSecret: string;
-    appKey: string;
-    clusterId: string;
-    supaBaseApiKey: string;
-    supaBaseUrl: string;
-
-    static createFrom(source: any = {}) {
-      return new Config(source);
-    }
-
-    constructor(source: any = {}) {
-      if ("string" === typeof source) source = JSON.parse(source);
-      this.appId = source["appId"];
-      this.appSecret = source["appSecret"];
-      this.appKey = source["appKey"];
-      this.clusterId = source["clusterId"];
-      this.supaBaseApiKey = source["supaBaseApiKey"];
-      this.supaBaseUrl = source["supaBaseUrl"];
-    }
-  }
 }
+
