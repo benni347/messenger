4dc2cf80716a4030478d6b93d6ff65d81d2b8415
Cedric Skwar <cdrc@skwar.me>
2023-05-18 12:03:08 +0200

    Started adding an icon for thee public chat room

--- frontend/wailsjs/go/models.ts
+++ frontend/wailsjs/go/models.ts
@@ -1,24 +1,27 @@
 export namespace main {
-  export class Config {
-    appId: string;
-    appSecret: string;
-    appKey: string;
-    clusterId: string;
-    supaBaseApiKey: string;
-    supaBaseUrl: string;
+	
+	export class Config {
+	    appId: string;
+	    appSecret: string;
+	    appKey: string;
+	    clusterId: string;
+	    supaBaseApiKey: string;
+	    supaBaseUrl: string;
+	
+	    static createFrom(source: any = {}) {
+	        return new Config(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.appId = source["appId"];
+	        this.appSecret = source["appSecret"];
+	        this.appKey = source["appKey"];
+	        this.clusterId = source["clusterId"];
+	        this.supaBaseApiKey = source["supaBaseApiKey"];
+	        this.supaBaseUrl = source["supaBaseUrl"];
+	    }
+	}
 
-    static createFrom(source: any = {}) {
-      return new Config(source);
-    }
-
-    constructor(source: any = {}) {
-      if ("string" === typeof source) source = JSON.parse(source);
-      this.appId = source["appId"];
-      this.appSecret = source["appSecret"];
-      this.appKey = source["appKey"];
-      this.clusterId = source["clusterId"];
-      this.supaBaseApiKey = source["supaBaseApiKey"];
-      this.supaBaseUrl = source["supaBaseUrl"];
-    }
-  }
 }
+
