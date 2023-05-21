7fc27d57148a26795e07177e29c8e83403a4586c
Cédric Skwar <cdrc@skwar.me>
2023-05-21 11:39:10 +0200

    Updated the note

--- frontend/src/public-chat-room.js
+++ frontend/src/public-chat-room.js
@@ -15,7 +15,7 @@ function validateChatRoomId(chatRoomId) {
 function addNote() {
   const noteP = document.createElement("p");
   noteP.innerHTML =
-    "Note: This is a public chat room. Anyone can see your messages. The messages are not stored in a database.";
+    "Note: This is a public chat room. Anyone can see your messages. The messages are stored in a database, unencrypted."; 
   const personDiv = document.querySelector(".person");
 
   personDiv.insertBefore(noteP, personDiv.querySelector(".signin"));
