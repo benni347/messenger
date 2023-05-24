59b0cfb7435ba8bf4c2258b5da7515edd086ca56
Cedric Skwar <cdrc@skwar.me>
2023-05-24 08:22:25 +0200

    stareted work on the new chat feature

---
+++ frontend/src/createChatroom.js
@@ -0,0 +1,17 @@
+'use strict';
+
+import { CreateChatRoomId } from "../wailsjs/go/main/App.js";
+
+function openNewChatRoomDialog() {
+  const newChatRoomDialog = document.getElementById("new_chat_room_dialog");
+  newChatRoomDialog.showModal();
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  const newChatRoomBtn = document.getElementById("new_chat_room_wrapper");
+  if (newChatRoomBtn) {
+    newChatRoomBtn.addEventListener("click", () => {
+      CreateChatRoomId();
+    });
+  }
+});
