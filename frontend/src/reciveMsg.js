d04ed8af009bdc6638b4808122c128664270e170
Cedric Skwar <cdrc@skwar.me>
2023-06-26 13:40:13 +0200

    Removed old unsused code which was commtended out anyway.

--- frontend/src/reciveMsg.js
+++ frontend/src/reciveMsg.js
@@ -1,72 +1,5 @@
 "use strict";
 
-import { ReciveFormatForJs } from "../wailsjs/go/main/App.js";
-
-// export class App {
-//   constructor() {
-//     // Initialize properties
-//     this.rabbitMqHost = this.getRabbitMqHost();
-//     this.rabbitMqAdmin = this.getRabbitMqAdmin();
-//     this.rabbitMqPassword = this.getRabbitMqPassword();
-//   }
-//
-//   getRabbitMqHost() {
-//     return GetRabbitMqHost();
-//   }
-//
-//   getRabbitMqAdmin() {
-//     return GetRabbitMqAdmin();
-//   }
-//
-//   getRabbitMqPassword() {
-//     return GetRabbitMqPassword();
-//   }
-//
-//   receive(channelId) {
-//     const amqpUrl = `amqp://${this.rabbitMqAdmin}:${encodeURIComponent(this.rabbitMqPassword)}@${this.rabbitMqHost}:5672/`;
-//
-//     amqp.connect(amqpUrl, (err, conn) => {
-//       if (err) {
-//         console.error("Failed to connect to RabbitMQ", err);
-//         return;
-//       }
-//
-//       conn.createChannel((err, channel) => {
-//         if (err) {
-//           console.error("Failed to open a channel", err);
-//           return;
-//         }
-//
-//         const queueName = channelId;
-//         channel.assertQueue(queueName, { durable: true }, (err) => {
-//           if (err) {
-//             console.error("Failed to declare a queue", err);
-//             return;
-//           }
-//
-//           channel.consume(
-//             queueName,
-//             (msg) => {
-//               if (msg !== null) {
-//                 console.log(msg.content.toString());
-//                 channel.ack(msg);
-//               }
-//             },
-//             { noAck: false }
-//           );
-//         });
-//       });
-//     });
-//   }
-// }
-//
-// // Usage
-// const app = new App();
-
-//
-// app.receive(getChatRoomId());
-//
-//
 const socket = new WebSocket("ws://s.rabbitmq.cedric.5y5.one/websocket");
 
 socket.onmessage = function(event) {
@@ -85,7 +18,5 @@ socket.onmessage = function(event) {
   messageDiv.appendChild(messageTextDiv);
   messageLog.appendChild(messageDiv);
   messageLog.scrollTop = messageLog.scrollHeight;
-  // let data = JSON.parse(event.data);
-  // console.log(`Received message from ${data.queue}: ${data.message}`);
 };
 
